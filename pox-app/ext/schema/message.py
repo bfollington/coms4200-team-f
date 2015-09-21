015b49449955ffba9d9c01927ae2eb05ddadb8a0
Ben Follington <bfollington@gmail.com>
2015-09-21 13:45:07 +1000

    Return get type method

--- pox-app/ext/schema/message.py
+++ pox-app/ext/schema/message.py
@@ -10,6 +10,9 @@ class Message(object):
         self.time = str(datetime.datetime.now())
         self.data = data
 
+    def get_type(self):
+        return self.__class__.__name__
+
     def to_dict(self):
 
         return {
