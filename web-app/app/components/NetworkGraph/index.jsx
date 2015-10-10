d06a106eb5b9ac66a76d9d91b845ff5416537317
Ben Follington <bfollington@gmail.com>
2015-10-10 19:35:24 +1000

    Support for removal of node and edges

--- web-app/app/components/NetworkGraph/index.jsx
+++ web-app/app/components/NetworkGraph/index.jsx
@@ -64,27 +64,47 @@ export default class NetworkGraph extends React.Component {
     };
 
     this.network = null;
+    this.networkAnimating = false;
     this.interval = setInterval(this.updateGraph.bind(this), 1000);
   }
 
   updateGraph() {
-    var hostNodes = _.keys(this.props.hosts).map(hostId => ({id: hostId, label: hostId, group: 0}));
-    var switchNodes = _.keys(this.props.switches).map(switchId => ({id: switchId, label: switchId, group: 1, value: this.props.switches[switchId].byte_rate}));
+    var hostNodes = _.keys(this.props.hosts)
+      .filter(hostId => this.props.hosts[hostId] !== null)
+      .map(hostId => ({id: hostId, label: hostId, group: 0}));
+    var switchNodes = _.keys(this.props.switches)
+      .filter(switchId => this.props.switches[switchId] !== null)
+      .map(switchId => ({id: switchId, label: switchId, group: 1, value: this.props.switches[switchId].byte_rate}));
 
-    var hostLinks = _.keys(this.props.hostLinks).map(hostLinkId => ({
-      id: hostLinkId,
-      from: this.props.hostLinks[hostLinkId]._switch,
-      to: this.props.hostLinks[hostLinkId].host,
-      group: 0
-    }));
+    var hostLinks = _.keys(this.props.hostLinks)
+      .filter(hostLinkId => this.props.hostLinks[hostLinkId].isUp)
+      .map(hostLinkId => ({
+        id: hostLinkId,
+        from: this.props.hostLinks[hostLinkId]._switch,
+        to: this.props.hostLinks[hostLinkId].host,
+        group: 0
+      }));
 
-    var switchLinks = _.keys(this.props.links).map(switchLinkId => ({
-      id: switchLinkId,
-      from: this.props.links[switchLinkId].from,
-      to: this.props.links[switchLinkId].to,
-      group: 1,
-      value: this.props.links[switchLinkId].stats.byte_rate
-    }));
+    var switchLinks = _.keys(this.props.links)
+      .filter(switchLinkId => this.props.links[switchLinkId].isUp)
+      .map(switchLinkId => ({
+        id: switchLinkId,
+        from: this.props.links[switchLinkId].from,
+        to: this.props.links[switchLinkId].to,
+        group: 1,
+        value: this.props.links[switchLinkId].stats.byte_rate
+      }));
+
+    var removedHostNodes = _.keys(this.props.hosts).filter(hostId => this.props.hosts[hostId] === null);
+    var removedSwitchNodes = _.keys(this.props.switches).filter(switchId => this.props.switches[switchId] === null);
+    var removedHostLinks = _.keys(this.props.hostLinks).filter(hostLinkId => !this.props.hostLinks[hostLinkId].isUp);
+    var removedSwitchLinks = _.keys(this.props.links).filter(switchLinkId => !this.props.links[switchLinkId].isUp);
+
+    this.nodes.remove(removedHostNodes);
+    this.nodes.remove(removedSwitchNodes);
+
+    this.edges.remove(removedHostLinks);
+    this.edges.remove(removedSwitchLinks);
 
     this.nodes.update(hostNodes);
     this.nodes.update(switchNodes);
@@ -92,11 +112,14 @@ export default class NetworkGraph extends React.Component {
     this.edges.update(hostLinks);
     this.edges.update(switchLinks);
 
-    var options = {
-      duration: 500,
-      easingFunction: "easeInOutQuad"
-    };
-    this.network.fit({animation:options});
+    if (!this.networkAnimating) {
+      var options = {
+        duration: 500,
+        easingFunction: "easeInOutQuad"
+      };
+      this.network.fit({animation:options});
+      this.networkAnimating = true;
+    }
   }
 
   componentDidMount() {
@@ -107,6 +130,7 @@ export default class NetworkGraph extends React.Component {
     };
 
     this.network = new vis.Network(container, graph, this.options);
+    this.network.on('animationFinished', () => { this.networkAnimating = false });
   }
 
   render() {
